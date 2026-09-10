c62d42d5c496878d820321bdccadc27df7cd9b7a
NaNVier <emcg1805@gmail.com>
2026-09-09 17:14:31 -0700

    prefabs

--- Monkey_Business_6.5.8/Assets/Scripts/Player.cs
+++ Monkey_Business_6.5.8/Assets/Scripts/Player.cs
@@ -8,6 +8,10 @@ public class Player : MonoBehaviour
 
     //[SerializeField] private float runSpeed = 5.0f; 
     //
+    //[SerializeField] private float runAcceleration = 30f;
+    //
+    //[SerializeField] private floar runDeceleration = 40f; 
+    //
     //[SerializeField] private InputActionAsset inputActions;
     //
     //InputAction moveAction;
@@ -114,7 +118,22 @@ public class Player : MonoBehaviour
     //{
     //   float hMovement = MoveInput.x;
     //
-    //   playerCharacter.linearVelocity = new Vector2(hMovement * runSpeed, playerCharacter.linearVelocity.y);
+    //   float targetSpeed = moveInput.x * runSpeed;
+    //
+    //   float speedChange;
+    //
+    //   if(Mathf.Abs(targetSpeed) > Mathf.Epsilon)
+    //   {
+    //      speedChange = runAcceleration; 
+    //   }
+    //   else
+    //   {
+    //      speedChange = runDeceleration;
+    //   }
+    //
+    //   float newSpeed = Mathf.MoveTowards(playerCharacter.linearVelocity.x,targetSpeed, speedChange * Time.deltaTime);
+    //
+    //   playerCharacter.linearVelocity = new Vector2(newSpeed, playerCharacter,linearVelocity.y);
     //}
 
     private void FixedUpdate()
